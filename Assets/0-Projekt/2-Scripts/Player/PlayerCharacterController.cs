d9f8edf40caa1a04e21b35dea3002d97ecf07543
UnrealGaimeDev <32965597+UnrealGaimeDev@users.noreply.github.com>
2026-05-27 21:29:35 +0200

    Camera movement and movement axis reversal to better suit demoScene

--- Assets/0-Projekt/2-Scripts/Player/PlayerCharacterController.cs
+++ Assets/0-Projekt/2-Scripts/Player/PlayerCharacterController.cs
@@ -30,9 +30,9 @@ public class PlayerCharacterController : MonoBehaviour
         // Move the Character
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
-        Vector3 direction = (Vector3.forward * y + Vector3.right * x).normalized + new Vector3(0,rb.linearVelocity.y,0);
+        Vector3 direction = (Vector3.right * -y + Vector3.forward * x).normalized + new Vector3(0,rb.linearVelocity.y,0);
         rb.linearVelocity = direction * stats.movmentSpeed;
-        animator.SetFloat("moveX", x);
-        animator.SetFloat("moveY", y);
+        animator.SetFloat("moveX", -y);
+        animator.SetFloat("moveY", x);
     }
 }
