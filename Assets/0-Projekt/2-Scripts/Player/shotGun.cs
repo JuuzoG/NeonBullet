57f5c777e2e17bd9b5b06e3c1f77c76d44bb66fb
Zenova <154024460+Voice45@users.noreply.github.com>
2026-06-12 12:32:00 +0200

    Revert "Merge branch 'main' of https://github.com/JuuzoG/NeonBullet"
    This reverts commit 9b08539611b3dfd518e3243a0d62dfb4ae7231f7, reversing
    changes made to 3389c22cf394583a8883de878f1e705a0057c239.

--- Assets/0-Projekt/2-Scripts/Player/shotGun.cs
+++ Assets/0-Projekt/2-Scripts/Player/shotGun.cs
@@ -3,24 +3,36 @@ using UnityEngine;
 public class shotGun : MonoBehaviour
 {
     public GameObject prefab;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F)) GunShot();
+    }
     public void GunShot()
     {
-        SpawnBullet(new Vector3(0.011f,  1.247f, 0.796f));
-        SpawnBullet(new Vector3(-0.094f, 1.252f, 0.796f));
-        SpawnBullet(new Vector3(0.134f,  1.255f, 0.796f));
-        SpawnBullet(new Vector3(0.014f,  1.125f, 0.796f));
-        SpawnBullet(new Vector3(0.007f,  1.374f, 0.796f));
-    }
+        GameObject Bullet = Instantiate(prefab, transform);
+        Bullet.transform.localPosition = new Vector3(0.011f, 1.247f, 0.796f);
+        Bullet.transform.localRotation = Quaternion.identity;
+        Bullet.transform.localScale = new Vector3(0.04474f, 0.10285f, 0.42595f);
 
-    void SpawnBullet(Vector3 localPos)
-    {
-        GameObject bullet = Instantiate(prefab, transform);
-        bullet.transform.localPosition = localPos;
-        bullet.transform.localRotation = Quaternion.identity;
-        bullet.transform.localScale = new Vector3(0.04474f, 0.10285f, 0.42595f);
+        GameObject Bullet2 = Instantiate(prefab, transform);
+        Bullet2.transform.localPosition = new Vector3(-0.094f, 1.252f, 0.796f);
+        Bullet2.transform.localRotation = Quaternion.identity;
+        Bullet2.transform.localScale = new Vector3(0.04474f, 0.10285f, 0.42595f);
 
-        bullet.transform.SetParent(null);
+        GameObject Bullet3 = Instantiate(prefab, transform);
+        Bullet3.transform.localPosition = new Vector3(0.134f, 1.255f, 0.796f);
+        Bullet3.transform.localRotation = Quaternion.identity;
+        Bullet3.transform.localScale = new Vector3(0.04474f, 0.10285f, 0.42595f);
 
-        bullet.GetComponent<ShotGunProjectile>()?.SetOwner(gameObject);
+        GameObject Bullet4 = Instantiate(prefab, transform);
+        Bullet4.transform.localPosition = new Vector3(0.014f, 1.125f, 0.796f);
+        Bullet4.transform.localRotation = Quaternion.identity;
+        Bullet4.transform.localScale = new Vector3(0.04474f, 0.10285f, 0.42595f);
+
+        GameObject Bullet5 = Instantiate(prefab, transform);
+        Bullet5.transform.localPosition = new Vector3(0.007f, 1.374f, 0.796f);
+        Bullet5.transform.localRotation = Quaternion.identity;
+        Bullet5.transform.localScale = new Vector3(0.04474f, 0.10285f, 0.42595f);
     }
 }
