a0fb693da443ff05f74e1f63d33ad8849a76b5f3
Zenova <154024460+Voice45@users.noreply.github.com>
2026-08-19 12:04:56 +0200

    inventar

--- Assets/0-Projekt/2-Scripts/Items/InventorySlot.cs
+++ Assets/0-Projekt/2-Scripts/Items/InventorySlot.cs
@@ -1,32 +1,43 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public class InventorySlot : MonoBehaviour
 {
     public Image itemDisplay;
+    public TMP_Text amount;
     private CollectedItem assignedItem;
 
     private void Start()
     {
-        ColorCorection();
+        Corection();
     }
 
     private void Update()
     {
-        ColorCorection();
+        Corection();
     }
 
-    private void ColorCorection()
+    private void Corection()
     {
-        if (itemDisplay.sprite == null) itemDisplay.color = Color.clear;
-        else itemDisplay.color = Color.white;
+        if (itemDisplay.sprite == null)
+        {
+            amount.text = "";
+            itemDisplay.color = Color.clear;
+        }
+        else
+        {
+            itemDisplay.color = Color.white;
+            amount.text = ""+ assignedItem.amount;
+        }
     }
 
     public void SetValues(CollectedItem item)
     {
         assignedItem = item;
         itemDisplay.sprite = (item == null) ? null : item.data.image;
+        //amount.text = ""+ item.amount;
     }
 
     public void GetPressed()
