d2c84589f8d0ddce1732b6c34cdb935fffafaebb
MarcelKuehn1 <Marcel.Kuehn@edu.bib.de>
2026-08-25 09:27:01 +0200

    PlayerSpeed

---
+++ Assets/0-Projekt/2-Scripts/VFX/Mesh_Trail.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using UnityEngine;
+
+public class Mesh_Trail : MonoBehaviour
+{
+    public float activeTime = 2f;
+
+    [Header("Mesh Related")]
+    public float meshRefreshRate = 0.1f;
+
+    private bool isTrailActive;
+    void Update()
+    {
+        if(Input.GetKeyDown (KeyCode.Space) && !isTrailActive)
+        {
+            isTrailActive = true;
+            StartCoroutine(ActivateTrail(activeTime));
+        }
+        
+    }
+
+    IEnumerator ActivateTrail (float timeActive)
+    {
+        while (timeActive  > 0)
+        {
+            timeActive -= meshRefreshRate;
+        }
+    }
+}
