19886188d595a6df166b7a690738f25a67f56c29
Zenova <154024460+Voice45@users.noreply.github.com>
2026-08-31 10:52:16 +0200

    asd

---
+++ Assets/0-Projekt/2-Scripts/Enemy/AllEnemysDead.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class AllEnemysDead : MonoBehaviour
+{
+    [SerializeField] private TriggerRelay trigger; 
+    [SerializeField] private string Tag = "Enemy";
+    [SerializeField] private GameObject door;
+
+    private void Start()
+    {
+        if (trigger != null)
+        {
+            trigger.OnEnter += HandleTriggerEnter;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (trigger != null)
+        {
+            trigger.OnEnter -= HandleTriggerEnter;
+        }
+    }
+
+    private void HandleTriggerEnter(Collider other)
+    {
+        if (!string.IsNullOrEmpty(Tag) || !other.CompareTag(Tag))
+        {
+            Destroy(door);
+        }
+    }
+}
--- Assets/0-Projekt/2-Scripts/Enemy/TriggerRelay.cs
+++ Assets/0-Projekt/2-Scripts/Enemy/TriggerRelay.cs
@@ -5,7 +5,7 @@ public class TriggerRelay : MonoBehaviour
 {
     public event Action<Collider> OnEnter;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         OnEnter?.Invoke(other);
     }
