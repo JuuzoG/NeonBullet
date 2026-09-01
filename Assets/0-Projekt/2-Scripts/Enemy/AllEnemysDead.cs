91329c5127a43826bf75514df1de05cae0adb313
Zenova <154024460+Voice45@users.noreply.github.com>
2026-09-01 11:45:41 +0200

    hv

--- Assets/0-Projekt/2-Scripts/Enemy/AllEnemysDead.cs
+++ Assets/0-Projekt/2-Scripts/Enemy/AllEnemysDead.cs
@@ -31,6 +31,7 @@ public class AllEnemysDead : MonoBehaviour
         if (other.CompareTag(enemyTag))
         {
             enemyCount++;
+            Debug.Log(enabled);
         }
     }
 
@@ -39,6 +40,7 @@ public class AllEnemysDead : MonoBehaviour
         if (other.CompareTag(enemyTag))
         {
             enemyCount--;
+            Debug.Log(enabled);
 
             if (enemyCount <= 0 && door != null)
             {
